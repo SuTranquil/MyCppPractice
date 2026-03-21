b7a4f06a66cabe71577702f89b3fd196b78767d5
SuTranquil <2791937275@qq.com>
2026-03-21 20:49:40 +0800

    cin输入用法

---
+++ p5_cin.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    //1.整型输入
+    // int a = 0;
+    // cout << "请输入一个整数：" << endl;
+    // cin >> a;
+    // cout << "整型变量a = " << a << endl;
+
+    //2.浮点型输入
+    // float f = 3.14f;
+    // cout << "请输入一个浮点数：" << endl;
+    // cin >> f;
+    // cout << "浮点型变量f = " << f << endl;
+
+    //3.字符型输入
+    // char ch = 'A';
+    // cout << "请输入一个字符：" << endl;
+    // cin >> ch;
+    // cout << "字符型变量ch = " << ch << endl;
+
+    //4.字符串输入
+    // string str = "Hello C++!";
+    // cout << "请输入一个字符串：" << endl;
+    // cin >> str;
+    // cout << "字符串变量str = " << str << endl;
+
+    //5.布尔类型输入
+    //非0即为true，0为false
+    bool flag = true;
+    cout << "请输入一个布尔值：" << endl;    
+    cin >> flag;
+    cout << "布尔变量flag = " << flag << endl;
+
+    system("pause");
+
+    
+    return 0;
+}
