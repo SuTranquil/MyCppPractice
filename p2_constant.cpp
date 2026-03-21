e887da5b7cc2b1fe1dd5ac874f878198bb5e4068
SuTranquil <2791937275@qq.com>
2026-03-21 11:17:59 +0800

    0321上午第一次学习C++

---
+++ p2_constant.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+// 常量
+// 1.宏常量
+#define Day 7
+//2. const修饰的常量
+const int Month = 12;
+
+int main()
+{
+
+    cout << "一周共有" << Day << "天。"<< endl;
+    cout << "一年共有" << Month << "个月。" << endl;
+
+    system("pause");
+    
+    return 0;
+}
